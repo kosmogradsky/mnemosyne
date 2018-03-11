5b5785b06a490cfc61064da7ab2c1e9523a1fdc2
Evgeniy Ivanov <kosmogradsky@gmail.com>
2018-03-11 12:33:01 +0300

    style fixes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -46,7 +46,11 @@ export default {
 <style scoped>
 
 .root {
-  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   padding-top: 26vh;
   display: flex;
   flex-direction: column;
